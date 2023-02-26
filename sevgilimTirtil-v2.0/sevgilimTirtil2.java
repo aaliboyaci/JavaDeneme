8517c0765744aa81ee67b7d8c3566c7c954d5d17
aaliboyaci <aaliboyaci@gmail.com>
2023-02-26 20:52:35 +0300

    Clean and refactor all the codes

--- sevgilimTirtil-v2.0/sevgilimTirtil2.java
+++ sevgilimTirtil-v2.0/sevgilimTirtil2.java
@@ -2,80 +2,78 @@
 import java.util.Scanner;
 
 public class sevgilimTirtil2 {
-    
-        //beş seçim kısmı olacağı için beş adet method yazıyorum
-        //olabilecek hemen hemen her durum için method oluşturup gerekli yerlerde o methodları call etmeyi denedim.
-        
-        public static int question1()
-        {
+
+    // beş seçim kısmı olacağı için beş adet method yazıyorum
+    // olabilecek hemen hemen her durum için method oluşturup gerekli yerlerde o
+    // methodları call etmeyi denedim.
+
+    public static int question1(Scanner aInput) {
         System.out.println("Soru 1: Sevgilin bir tırtıl olsa onu sever miydin?" + "Evet = 1, Hayır = 2");
-         Scanner userInput = new Scanner(System.in);
-         int userDecision = userInput.nextInt();
-            return userDecision;
-        }
 
-        public static int question2()
-        {
-        System.out.println("Soru 2: Sevgilin bir tırtıl olarak kozaya giriyor sonrasında kozadan çıkana kadar onu sever miydin?" + "Evet = 1, Hayır = 2");
-         Scanner userInput = new Scanner(System.in);
-         int userDecision = userInput.nextInt();
-            return userDecision;
-        }
-        public static int question3()
-        {
-        System.out.println("Soru 3: Sevgilin kozadan çirkin berbat kapkara bir kelebek olarak çıktı. Ona olan sevgin azalır mıydı?" + "Evet = 1, Hayır = 2");
-         Scanner userInput = new Scanner(System.in);
-         int userDecision = userInput.nextInt();
-            return userDecision;
-        }
+        int userDecision = aInput.nextInt();
+        return userDecision;
+    }
 
-        public static int question4()
-        {
+    public static int question2(Scanner aInput) {
+        System.out.println(
+                "Soru 2: Sevgilin bir tırtıl olarak kozaya giriyor sonrasında kozadan çıkana kadar onu sever miydin?"
+                        + "Evet = 1, Hayır = 2");
+        int userDecision = aInput.nextInt();
+        return userDecision;
+    }
+
+    public static int question3(Scanner aInput) {
+        System.out.println(
+                "Soru 3: Sevgilin kozadan çirkin berbat kapkara bir kelebek olarak çıktı. Ona olan sevgin azalır mıydı?"
+                        + "Evet = 1, Hayır = 2");
+        int userDecision = aInput.nextInt();
+        return userDecision;
+    }
+
+    public static int question4(Scanner aInput) {
         System.out.println("Soru 4: Sevgilin ölene kadar başka kızlara bakar mıydın?" + "Evet = 1, Hayır = 2");
-         Scanner userInput = new Scanner(System.in);
-         int userDecision = userInput.nextInt();
-            return userDecision;
-        }
+        int userDecision = aInput.nextInt();
+        return userDecision;
+    }
 
+    // soular ve seçimlerin dışında kalan durumların methodları
 
-        //soular ve seçimlerin dışında kalan durumların methodları
+    public static void losingScenario() {// oyunun kaybedildiği senaryolarda kullanılacak method
+        System.out.println("Oyunu kaybettin ve kendinden utanmalısın.");
+    }
 
+    public static int endingGame(Scanner aInput) { // oyun sonunda yeniden oynamak istiyor mu
+        System.out.println("oyunu tekrar oynamak istiyor musun?");
+        System.out.println("Tekrar oynamak için 1, çıkmak için 2 yaz ve enter'a bas");
+        int replay = aInput.nextInt();
+        return replay;
+    }
 
-        public static void losingScenario(){//oyunun kaybedildiği senaryolarda kullanılacak method
-            System.out.println("Oyunu kaybettin ve kendinden utanmalısın.");    
-        }
-        public static int endingGame(){ //oyun sonunda yeniden oynamak istiyor mu
-            Scanner userInput = new Scanner(System.in);
-            System.out.println("oyunu tekrar oynamak istiyor musun?");
-            System.out.println("Tekrar oynamak için 1, çıkmak için 2 yaz ve enter'a bas");
-            int replay = userInput.nextInt();
-            return replay ;
-        }
-        public static void winningScenario(){
-            System.out.println("! ! ! ! ! ! TEBRİKLER ! ! ! !");
-            System.out.println("Oyunu kazandın tebrikler, sevgiline olan bağlılığını kanıtladın!! ");
-            System.out.println(" ");
-        }
-
-
+    public static void winningScenario() {
+        System.out.println("! ! ! ! ! ! TEBRİKLER ! ! ! !");
+        System.out.println("Oyunu kazandın tebrikler, sevgiline olan bağlılığını kanıtladın!! ");
+        System.out.println(" ");
+    }
 
     /**
      * @param args
      */
-    public static void main(String args[]){
+    public static void main(String args[]) {
 
-        boolean reloadGame = (true) ;
+        boolean reloadGame = (true);
         boolean isTheGameOn = (true);
-        Scanner userInput = new Scanner (System.in);
-        
-        
-        //oyunun kendisi yani ana iskelet burada başlıyor
-        //soruları/seçimleri ayrı methodalara böldüm
-        //iç içe if else kullanmaktansa oyunu tek çizgi halinde devam eden ya da kaybedince ana döngüyü bitiren bir formata soktum.
-        //bu mantık ile ağaç gibi dallanma yapmadığım için çok daha karmaşık (daha sonra yapmayı planladığım bandersnatch oyunu gibi),
-        //aşırı kompleks dallandırmaları kolaylıkla yapabileceğimi fakettim. çünkü aslında lineer bir hat oluşturabiliyorum böyle.
-        while (reloadGame){
-            while (isTheGameOn){
+        Scanner userInput = new Scanner(System.in);
+
+        // oyunun kendisi yani ana iskelet burada başlıyor
+        // soruları/seçimleri ayrı methodalara böldüm
+        // iç içe if else kullanmaktansa oyunu tek çizgi halinde devam eden ya da
+        // kaybedince ana döngüyü bitiren bir formata soktum.
+        // bu mantık ile ağaç gibi dallanma yapmadığım için çok daha karmaşık (daha
+        // sonra yapmayı planladığım bandersnatch oyunu gibi),
+        // aşırı kompleks dallandırmaları kolaylıkla yapabileceğimi fakettim. çünkü
+        // aslında lineer bir hat oluşturabiliyorum böyle.
+        while (reloadGame) {
+            while (isTheGameOn) {
 
                 System.out.println("");
                 System.out.println("Sevgilim Tirtil Oyununa Hoşgeldin");
@@ -84,49 +82,47 @@ public class sevgilimTirtil2 {
                 System.out.println(" ");
                 System.out.println(" ");
 
-                int userDecision = question1();   //birinci soru için methodu çağırdım
-             if (userDecision == 1)
-                    { }
-                else {
+                int userDecision = question1(userInput); // birinci soru için methodu çağırdım
+                if (userDecision == 2) {
                     losingScenario();
-                        isTheGameOn = false;
-                        break;                
+                    isTheGameOn = false;
+                    break;
                 }
 
-                userDecision = question2();   //ikinci soru için methodu çağırdım
-             if (userDecision == 1)
-                    {  }
-                else {
+                userDecision = question2(userInput); // ikinci soru için methodu çağırdım
+                if (userDecision == 2) {
                     losingScenario();
-                        isTheGameOn = (false); 
-                        break;
-                }
-            
-                userDecision = question3();   //üçüncü soru için methodu çağırdım
-             if (userDecision == 1)
-                    {  }
-                else {
-                    winningScenario(); //burada hayır dendiğinde otomatik kazanıyor oyuncu (bilerek böyle yaptım)
-                        isTheGameOn = (false); 
-                        break;
+                    isTheGameOn = (false);
+                    break;
                 }
 
-                userDecision = question4();   //dördüncü soru için methodu çağırdım
-                if (userDecision == 1)
-                       { losingScenario();
-                        isTheGameOn = (false); 
-                        break; }
-                   else {
-                       winningScenario();
-                           isTheGameOn = (false); 
-                           break;
-                   }
+                userDecision = question3(userInput); // üçüncü soru için methodu çağırdım
+                if (userDecision == 2) {
+                    winningScenario(); // burada hayır dendiğinde otomatik kazanıyor oyuncu (bilerek böyle yaptım)
+                    isTheGameOn = (false);
+                    break;
+                }
+
+                userDecision = question4(userInput); // dördüncü soru için methodu çağırdım
+                if (userDecision == 1) {
+                    losingScenario();
+                    isTheGameOn = (false);
+                    break;
+                } else {
+                    winningScenario();
+                    isTheGameOn = (false);
+                    break;
+                }
             }
 
-            //oyun bitti, tekrar oynamak istiyor mu, kullanıcıya soruyorum.
-            int replay = endingGame();
-            if (replay == 1){  reloadGame = true; isTheGameOn=true;}
-            else {  reloadGame = false;}
+            // oyun bitti, tekrar oynamak istiyor mu, kullanıcıya soruyorum.
+            int replay = endingGame(userInput);
+            if (replay == 1) {
+                reloadGame = true;
+                isTheGameOn = true;
+            } else {
+                reloadGame = false;
+            }
         }
     }
 
